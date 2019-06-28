ef58036a5fb35605a339004f2061d6e359aa36c7
Joshua Kelly <jdkelly2015@gmail.com>
2019-06-28 00:40:29 -0400

    started over with game2.js. going even smaller this time. setting each variable, writing each function, wrote a for loop to control winner and loser screens.

---
+++ assets/javascript/game2.js
@@ -0,0 +1,54 @@
+//theme: FINISH THE MEME!!
+//Questions: 1- It is Wednesday my dudes... (AHHHHHHHHHHHHH!!!!!, Hump Dayyy!!, Middle of the Week Sneak Peek, Only 234.5 days until Halloween)
+//Questions: 2- Hurricane Katrina?! More like... (Hurricane Harvey!!, Issa Knife, Look at all those chickens!, Hurricane Tor- Tilla!!)
+//Questions: 3- On May 28th, 2015, the 88th Scripps National Spelling Bee was held at the Gaylord National Resort & Convention Center in National Harbor, Maryland. During the competition, Louisville, Mississippi teenager Dev Jaiswall placed 5th after incorrectly spelling 
+//Questions: 4- Why you always.. (Playing, Dancing, Eating, Lying)
+
+// show one question 
+
+// $("#questionDiv").text("Finish this famous quote: 'It is Wednesday my dudes...' ");
+// $("#questionDiv").text("Fill in the blank: Hurricane Katrina?! More like Hurricane ____ ")
+// $("#questionDiv").text("On May 28th, 2015, the 88th Scripps National Spelling Bee was held at the Gaylord National Resort & Convention Center in National Harbor, Maryland. During the competition, Louisville, Mississippi teenager Dev Jaiswall placed 5th after incorrectly spelling which word?") 
+// $("#questionDiv").text("Complete the song lyrics: 'Why you always..'");
+
+// until an answer 
+
+// var questionOneAnswers = ["AHHHHHHHHHHHHH!!!!!", "Hump Dayyy!!", "Middle of the Week Sneak Peek", "Only 234.5 days until Halloween"];
+// var questionTwoAnswers = ["Hurricane Harvey!!", "Issa Knife", "Look at all those chickens!", "Hurricane Tor- Tilla!!"];
+// var questionThreeAnswers = ["iridocyclitis", "Diplodocus", "idiosyncratic", "sesquipedalian"]
+// var questionFourAnswers = ["Playing", "Dancing", "Eating", "Lying"]
+// $("#buttonOne").text(questionOneAnswers[0]);
+// $("#buttonTwo").text(questionOneAnswers[1]);
+// $("#buttonThree").text(questionOneAnswers[2]);
+// $("#buttonFour").text(questionOneAnswers[3]);
+
+// is chosen
+
+// var correctAnswer = true ;
+// $(".btn").on("click", function(){   
+//  if(this.answer === correctAnswer){
+//      var winnerScreen = function(){
+//          pause timer
+//          $("#questionDiv").text("Correct!");
+//          display a GIPHY in place of the button group
+// }
+// }else{ 
+//      var loserScreen = function (){
+//      pause timer
+//      $("#questionDiv").text("Nope!");
+//      display a GIPHY in place of the button group with "The correct answer is: "
+// }
+// }
+// });
+
+
+// if loop that reads a value set by an array method on click to determine if the button clicked was correct or not
+
+//after a few seconds display next question without user input
+
+// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+//if time equals 0 display "time is up", show correctAnswer, wait 2 seconds, show next question
+// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+//if wrongAnswer = 0 display "youre wrong", dis play correct answer, wait 2 seconds, show next question
+// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+// incorrectAnswers , correctAnswers. restart button
