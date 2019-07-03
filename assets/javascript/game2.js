e00397b3e5b5ae3c7fc3fa5c8f5391b1c4870b89
Joshua Kelly <jdkelly2015@gmail.com>
2019-07-02 23:45:10 -0400

    ugh.

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,54 +1,110 @@
-//theme: FINISH THE MEME!!
-//Questions: 1- It is Wednesday my dudes... (AHHHHHHHHHHHHH!!!!!, Hump Dayyy!!, Middle of the Week Sneak Peek, Only 234.5 days until Halloween)
-//Questions: 2- Hurricane Katrina?! More like... (Hurricane Harvey!!, Issa Knife, Look at all those chickens!, Hurricane Tor- Tilla!!)
-//Questions: 3- On May 28th, 2015, the 88th Scripps National Spelling Bee was held at the Gaylord National Resort & Convention Center in National Harbor, Maryland. During the competition, Louisville, Mississippi teenager Dev Jaiswall placed 5th after incorrectly spelling 
-//Questions: 4- Why you always.. (Playing, Dancing, Eating, Lying)
+// page loads to a header and a single start button
+// on start button click reveal quiz questions and timer
+// each questions has four options to answer with, only one answer has the value of true the rest are false
+// timer starts at 180 seconds and counts down to zero
 
-// show one question 
+// on zero seconds the game ends and will reveal the quiz stats
+// quiz stats are questions correct: and questions wrong: 
+// questions correct: ++ per checked radio box with the true value
+// questions wrong: ++ per checked radio box with the false value
+// if a radio box is not checked then it is counted as false and ++ a wrong answer 
+// also reveal a button to start the game over
+// start over button on click will clear radio boxes, and set stats to 0, and restart timer 
 
-// $("#questionDiv").text("Finish this famous quote: 'It is Wednesday my dudes...' ");
-// $("#questionDiv").text("Fill in the blank: Hurricane Katrina?! More like Hurricane ____ ")
-// $("#questionDiv").text("On May 28th, 2015, the 88th Scripps National Spelling Bee was held at the Gaylord National Resort & Convention Center in National Harbor, Maryland. During the competition, Louisville, Mississippi teenager Dev Jaiswall placed 5th after incorrectly spelling which word?") 
-// $("#questionDiv").text("Complete the song lyrics: 'Why you always..'");
+// click submit button to calculate correct and false questions
+// if true ++ correct answer
+// else ++ wrong answer
+// these numbers are displayed at the end of quiz
+// end of game is determined at 0 seconds or on submit button click
+// end of the quiz reveals game stats. questions correct/wrong, and a start over button
 
-// until an answer 
+$(document).ready(function () {
+    var seconds = 180;
+    var correct = 0;
+    var incorrect = 0;
+    $("#quesitonDiv").hide();
+    $("#timerDiv").hide();
+    $("form").hide();
+    $("#winLoss").hide();
+    $("#start-button").on("click", function () {
+        setInterval(function () {
 
-// var questionOneAnswers = ["AHHHHHHHHHHHHH!!!!!", "Hump Dayyy!!", "Middle of the Week Sneak Peek", "Only 234.5 days until Halloween"];
-// var questionTwoAnswers = ["Hurricane Harvey!!", "Issa Knife", "Look at all those chickens!", "Hurricane Tor- Tilla!!"];
-// var questionThreeAnswers = ["iridocyclitis", "Diplodocus", "idiosyncratic", "sesquipedalian"]
-// var questionFourAnswers = ["Playing", "Dancing", "Eating", "Lying"]
-// $("#buttonOne").text(questionOneAnswers[0]);
-// $("#buttonTwo").text(questionOneAnswers[1]);
-// $("#buttonThree").text(questionOneAnswers[2]);
-// $("#buttonFour").text(questionOneAnswers[3]);
+            $("#timer").text("Time Remaining: " + seconds--);
+        }, 1000);
+        $("#quesitonDiv").show();
+        $("#timerDiv").show();
+        $("form").show();
 
-// is chosen
+        // console.log("hello")
 
-// var correctAnswer = true ;
-// $(".btn").on("click", function(){   
-//  if(this.answer === correctAnswer){
-//      var winnerScreen = function(){
-//          pause timer
-//          $("#questionDiv").text("Correct!");
-//          display a GIPHY in place of the button group
-// }
-// }else{ 
-//      var loserScreen = function (){
-//      pause timer
-//      $("#questionDiv").text("Nope!");
-//      display a GIPHY in place of the button group with "The correct answer is: "
-// }
-// }
-// });
+    });
+
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        // console.log("i clicked a button ");
+
+        var answerOne = $("input[name=answerOne]:checked").val();
+        if (answerOne === "aaahhhhhh") {
+            correct++
+            console.log(correct++);
+        } else {
+            incorrect++
+            console.log(incorrect++);
+        };
+        console.log(answerOne);
+
+        var answerTwo = $("input[name=answerTwo]:checked").val();
+        if (answerTwo === "hurricane tortilla!") {
+            correct++
+            console.log(correct++);
+        } else {
+            incorrect++
+            console.log(incorrect++);
+        };
+        console.log(answerTwo);
+
+        var answerThree = $("input[name=answerThree]:checked").val();
+        if (answerThree === "hurricane tortilla!") {
+            correct++
+            console.log(correct++);
+        } else {
+            incorrect++
+            console.log(incorrect++);
+        };
+        console.log(answerThree);
+
+        var answerFour = $("input[name=answerFour]:checked").val();
+        if (answerFour === "lying") {
+            correct++
+            console.log(correct++);
+        } else {
+            incorrect++
+            console.log(incorrect++);
+        };
+        console.log(answerFour);
+        $("#start-button").hide();
+        $("#winLoss").show();
+        $("#quesitonDiv").hide();
+        $("#timerDiv").hide();
+        $("form").hide();
+        var newDiv = $("<div>")
+        $(newDiv).text("You got" + correct + "correct." +  "You got" + incorrect + "incorrect.")
+        $("#winLoss").append(newDiv);
+    });
 
 
-// if loop that reads a value set by an array method on click to determine if the button clicked was correct or not
 
-//after a few seconds display next question without user input
+});
 
-// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-//if time equals 0 display "time is up", show correctAnswer, wait 2 seconds, show next question
-// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
-//if wrongAnswer = 0 display "youre wrong", dis play correct answer, wait 2 seconds, show next question
-// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-// incorrectAnswers , correctAnswers. restart button
+
+
+
+
+
+
+
+
+
+
+
+
